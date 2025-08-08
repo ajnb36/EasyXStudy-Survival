92d407a0f02ebb1dfbd2f9419c9c67ba34d91dfc
ajnb36 <97371685+ajnb36@users.noreply.github.com>
2025-08-08 16:49:36 +0800

    素材

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <graphics.h>
+#include <iterator>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    initgraph(1280, 720);
+    BeginBatchDraw();
+
+    IMAGE img;
+    loadimage(&img, TEXT(""));
+
+    bool isRunning = true;
+    while (isRunning) {
+        DWORD beginTime = GetTickCount();
+        ExMessage msg;
+        while (peekmessage(&msg)) {
+
+        }
+
+        FlushBatchDraw();
+        DWORD endTime = GetTickCount();
+        DWORD divTime = endTime - beginTime;
+        if (divTime < 1000 / 144) {
+            Sleep(1000 / 144 - divTime);
+        }
+
+    }
+
+
+    EndBatchDraw();
     return 0;
 }
